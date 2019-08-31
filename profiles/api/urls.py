cac07219d56daf7fd5d351c5e97b054d34839943
ArRosid <ahmadrosid30121997@gmail.com>
2019-09-01 06:58:24 +0700

    Built API using Read Only Model ViewSet

--- profiles/api/urls.py
+++ profiles/api/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
 from . import views
 
+profile_list = views.ProfileViewSet.as_view({"get":"list"})
+profile_detail = views.ProfileViewSet.as_view({"get":"retrieve"})
+
 urlpatterns = [
-    path('profiles/', views.ProfileList.as_view(), name='profile-list'),
+    # path('profiles/', views.ProfileList.as_view(), name='profile-list'),
+    path("profiles/", profile_list, name="profile-list"),
+    path('profiles/<int:pk>/', profile_detail, name="profile-detail"),
 ]
--- profiles/api/views.py
+++ profiles/api/views.py
@@ -1,9 +1,15 @@
 from rest_framework import generics
+from rest_framework.viewsets import ReadOnlyModelViewSet
 from rest_framework.permissions import IsAuthenticated
 from profiles import models
 from . import serializers
 
-class ProfileList(generics.ListAPIView):
+# class ProfileList(generics.ListAPIView):
+#     queryset = models.Profile.objects.all()
+#     serializer_class = serializers.ProfileSerializer
+#     permission_classes = [IsAuthenticated]
+
+class ProfileViewSet(ReadOnlyModelViewSet):
     queryset = models.Profile.objects.all()
     serializer_class = serializers.ProfileSerializer
     permission_classes = [IsAuthenticated]
