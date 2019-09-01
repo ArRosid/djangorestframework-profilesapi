951fae4adf00579be18354bd5f54fa93d36b5bea
ArRosid <ahmadrosid30121997@gmail.com>
2019-09-01 07:07:13 +0700

    Dynamic urls using rest_framework routers

--- profiles/api/urls.py
+++ profiles/api/urls.py
@@ -1,11 +1,16 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
 from . import views
 
-profile_list = views.ProfileViewSet.as_view({"get":"list"})
-profile_detail = views.ProfileViewSet.as_view({"get":"retrieve"})
+router = DefaultRouter()
+router.register(r"profiles", views.ProfileViewSet)
+
+# profile_list = views.ProfileViewSet.as_view({"get":"list"})
+# profile_detail = views.ProfileViewSet.as_view({"get":"retrieve"})
 
 urlpatterns = [
     # path('profiles/', views.ProfileList.as_view(), name='profile-list'),
-    path("profiles/", profile_list, name="profile-list"),
-    path('profiles/<int:pk>/', profile_detail, name="profile-detail"),
+    # path("profiles/", profile_list, name="profile-list"),
+    # path('profiles/<int:pk>/', profile_detail, name="profile-detail"),
+    path("", include(router.urls))
 ]
--- profiles/api/views.py
+++ profiles/api/views.py
@@ -1,5 +1,5 @@
 from rest_framework import generics
-from rest_framework.viewsets import ReadOnlyModelViewSet
+from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated
 from profiles import models
 from . import serializers
@@ -9,7 +9,7 @@ from . import serializers
 #     serializer_class = serializers.ProfileSerializer
 #     permission_classes = [IsAuthenticated]
 
-class ProfileViewSet(ReadOnlyModelViewSet):
+class ProfileViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = models.Profile.objects.all()
     serializer_class = serializers.ProfileSerializer
     permission_classes = [IsAuthenticated]
