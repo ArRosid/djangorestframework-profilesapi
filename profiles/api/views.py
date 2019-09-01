7b98039b15c287f5c0b92ad16f5cdb36f8bd6017
ArRosid <ahmadrosid30121997@gmail.com>
2019-09-01 07:12:01 +0700

    List, Retrieve, Update in one class

--- profiles/api/views.py
+++ profiles/api/views.py
@@ -1,15 +1,25 @@
-from rest_framework import generics
-from rest_framework import viewsets
-from rest_framework.permissions import IsAuthenticated
+from rest_framework import generics, mixins, viewsets
+from rest_framework import permissions
 from profiles import models
 from . import serializers
+from . import permissions as custom_permissions
 
 # class ProfileList(generics.ListAPIView):
 #     queryset = models.Profile.objects.all()
 #     serializer_class = serializers.ProfileSerializer
 #     permission_classes = [IsAuthenticated]
 
-class ProfileViewSet(viewsets.ReadOnlyModelViewSet):
+# class ProfileViewSet(viewsets.ReadOnlyModelViewSet):
+#     queryset = models.Profile.objects.all()
+#     serializer_class = serializers.ProfileSerializer
+#     permission_classes = [IsAuthenticated]
+
+class ProfileViewSet(mixins.UpdateModelMixin,
+                    mixins.ListModelMixin,
+                    mixins.RetrieveModelMixin,
+                    viewsets.GenericViewSet):
+
     queryset = models.Profile.objects.all()
     serializer_class = serializers.ProfileSerializer
-    permission_classes = [IsAuthenticated]
+    permission_classes = [permissions.IsAuthenticated,
+                        custom_permissions.IsOwnProfileOrReadOnly]
